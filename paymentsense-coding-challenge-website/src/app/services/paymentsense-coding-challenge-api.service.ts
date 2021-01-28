70dd9a566d55a703618ead665f22a6006d1c4f0d
Jayesh Karathiya <jayeshsagar@hotmail.com>
2021-01-28 16:03:03 +0000

    Updated Packages & code cleanup

--- paymentsense-coding-challenge-website/src/app/services/paymentsense-coding-challenge-api.service.ts
+++ paymentsense-coding-challenge-website/src/app/services/paymentsense-coding-challenge-api.service.ts
@@ -1,14 +1,15 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PaymentsenseCodingChallengeApiService {
-  constructor(private httpClient: HttpClient) {}
+  constructor(private httpClient: HttpClient) { }
 
   public getHealth(): Observable<string> {
-    return this.httpClient.get('https://localhost:44341/health', { responseType: 'text' });
+    return this.httpClient.get(environment.apiUrl + '/health', { responseType: 'text' });
   }
 }
--- paymentsense-coding-challenge-website/src/environments/environment.prod.ts
+++ paymentsense-coding-challenge-website/src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'https://localhost:5001',//This need to replace with production url.
+  apiVersion: "v1"
 };
